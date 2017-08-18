dce8f21b31e8c63155ec063bc4b0c32b4222014c
Lucas Soares <lsoares267@gmail.com>
2017-08-18 09:22:52 -0300

    Upando

---
+++ Assets/Scripts/Editor/TableBehaviourEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TableBehaviour))]
+public class NewBehaviourScript : Editor
+{
+    int value;
+    int index;
+    public override void OnInspectorGUI()
+    {
+        TableBehaviour tableBehaviour = target as TableBehaviour;
+        EditorGUILayout.LabelField("Add", EditorStyles.boldLabel);
+        value = EditorGUILayout.IntField("Value", value);
+
+        if (GUILayout.Button("Add"))
+        {
+            tableBehaviour.AddToList(tableBehaviour.entryList.Count.ToString(), value);
+        }
+
+        EditorGUILayout.LabelField("Remove", EditorStyles.boldLabel);
+        index = EditorGUILayout.IntField("Value", index);
+        if (GUILayout.Button("Remove"))
+        {
+            tableBehaviour.RemoveFromList(index);
+        }
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.LabelField("List", EditorStyles.boldLabel);
+        for (int i = 0; i < tableBehaviour.entryList.Count; i++)
+        {
+            EditorGUILayout.FloatField(tableBehaviour.entryList[i].key, tableBehaviour.entryList[i].value);
+        }
+        EditorGUI.EndDisabledGroup();
+    }
+}
---
+++ Assets/Scripts/TableBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableBehaviour : MonoBehaviour
+{  
+    public List<Entry> entryList  = new List<Entry>(0);
+
+    /// <summary>
+    /// Metodo para adicionar elemento na lista
+    /// </summary>
+    /// <param name="key">Recebe uma key</param>
+    /// <param name="value">Recebe o valor</param>
+    public void AddToList(string key, int value)
+    {
+        Entry entry = new Entry();
+        entry.key = key;
+        entry.value = value;
+        entryList.Add(entry);
+    }
+
+    /// <summary>
+    /// Metodo para remover elemento da lsita
+    /// </summary>
+    /// <param name="index">Recebe o indice do elemento</param>
+    public void RemoveFromList(int index)
+    {
+        entryList.RemoveAt(index);
+    }
+}
+
+/// <summary>
+/// Classe Entry para os dados
+/// </summary>
+public class Entry
+{
+    public string key;
+    public int value;
+}
